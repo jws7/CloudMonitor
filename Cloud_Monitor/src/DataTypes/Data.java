0fb9034fcc7e742d06be64a15a554661938d869c
James Smith <james.w.smith@st-andrews.ac.uk>
2011-11-23 22:52:54 +0000

    Added License notices

--- Cloud_Monitor/src/DataTypes/Data.java
+++ Cloud_Monitor/src/DataTypes/Data.java
@@ -3,6 +3,25 @@ package DataTypes;
 import java.io.Serializable;
 import java.util.Date;
 
+/*
+ * Copyright (C) 2009-2010 School of Computer Science, University of St Andrews. All rights reserved.
+ * Project Homepage: http://jws7.net/cloudmonitor
+ *
+ * CloudMonitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * CloudMonitor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with CloudMonitor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 public abstract class Data implements Serializable {
 
 	private static final long serialVersionUID = -6235540795909992564L;
